6b12467d3e37ce7b720a0cef5631ab628233aa98
huynhan147 <huyitptit2015@gmail.com>
2018-05-28 17:26:04 +0700

    Pass Data to Your Views

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $task = [
+        'Go to store',
+        'Test Laravel',
+        'Laravel Basic'
+
+
+    ];
+    return view('welcome',compact('task'));
+});
+Route::get('about',function(){
+    return view('about');
+});
